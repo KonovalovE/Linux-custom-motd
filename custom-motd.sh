8dfc7bdf51656bcc542093c60f0dcf0d27938fd2
Konovalov Evgeny <el_admin@bk.ru>
2023-05-18 20:21:11 +0300

    Update custom-motd.sh

--- custom-motd.sh
+++ custom-motd.sh
@@ -9,23 +9,15 @@
 #	E-MAIL: kev@modbon.ru
 #	WWW: https://modbon.ru
 #
-#	VERSION: 1.1
-#	CREATED: 28/04/2023
+#	VERSION: 1.7
+#	CREATED: 18/05/2023
 # =======================================================
 
 ## version
-VERSION="MOTD v1.1"
+VERSION="MOTD v1.7"
 
 ## configuration and logfile
-ENVFILE="/root/.CNTEC/environment.cfg"
-
-## enable system related information about your system
-SYSTEM_INFO="1"             # show system information
-ENVIRONMENT_INFO="1"        # show environement information
-STORAGE_INFO="1"            # show storage information
-USER_INFO="1"               # show some user infomration
-
-#### color schemes
+ENVFILE="/opt/.CNTEC/environment.cfg"
 
 ## MOTD theme scheme
 F1="\033[0;32m"
@@ -33,15 +25,8 @@ F2="\033[0;32m"
 F3="\033[1;32m"
 F4="\033[0;31m"
 
-## don't start as root
-if [ $(whoami) != root ]; then
-    cat /etc/motd
-    exit 0
-fi
-
 ## create .environment file if not exist
 function createenv {
-
 
         echo "We want to assign a function name for $(hostname --fqdn)"
         echo
@@ -53,19 +38,53 @@ function createenv {
         read SYSSLA
         echo -n "Brief description of the system: [${4}] "
         read SYSNOTE
+        echo -n "Only for ROOT (Y/N): [${5}] "
+        read ONLY_ROOT
+        echo -n "Show system information (Y/N): [${6}] "
+        read SYSTEM_INFO
+        echo -n "Show environement information (Y/N): [${7}] "
+        read ENVIRONMENT_INFO
+        echo -n "Show storage information (Y/N): [${8}] "
+        read STORAGE_INFO
+        echo -n "Show some user infomration (Y/N): [${9}] "
+        read USER_INFO
         rm -rf $ENVFILE
         mkdir -p $(dirname $ENVFILE)
         touch $ENVFILE
-        chmod 600 $ENVFILE
+        chmod 644 $ENVFILE
        	echo "SYSENV=\"$SYSENV\"" >> $ENVFILE
        	echo "SYSFUNCTION=\"$SYSFUNCTION\"" >> $ENVFILE
         echo "SYSSLA=\"$SYSSLA\"" >> $ENVFILE
+        echo "SYSNOTE=\"$SYSNOTE\"" >> $ENVFILE
+        echo "ONLY_ROOT=\"$ONLY_ROOT\"" >> $ENVFILE
+        echo "SYSTEM_INFO=\"$SYSTEM_INFO\"" >> $ENVFILE
+        echo "ENVIRONMENT_INFO=\"$ENVIRONMENT_INFO\"" >> $ENVFILE
+        echo "STORAGE_INFO=\"$STORAGE_INFO\"" >> $ENVFILE
+        echo "USER_INFO=\"$USER_INFO\"" >> $ENVFILE
 }
+
+if [ ! -f $ENVFILE ]; then
+	createenv ;
+fi
+source $ENVFILE
+if [ -z "${SYSFUNCTION}" ] || [ -z "${SYSENV}" ] || [ -z "${SYSSLA}" ] || [ -z "${SYSNOTE}" ] || [ -z "${ONLY_ROOT}" ] || [ -z "${SYSTEM_INFO}" ] || [ -z "${ENVIRONMENT_INFO}" ] || [ -z "${STORAGE_INFO}" ] || [ -z "${USER_INFO}" ]; then
+	rm $ENVFILE
+	createenv ;	
+fi
+
+## don't start as root
+if [[ "$ONLY_ROOT" = "Y" || "$ONLY_ROOT" = "y" ]]; then
+
+	if [ $(whoami) != root ]; then
+		cat /etc/motd
+		exit 0
+	fi
+fi
 
 ## System Info
 function show_system_info () {
 
-    if [ "$SYSTEM_INFO" = "1" ]; then
+    if [[ "$SYSTEM_INFO" = "Y" || "$SYSTEM_INFO" = "y" ]]; then
 
         HOSTNAME=$(hostname --fqdn)
         IP=$(/sbin/ip -o -4 addr list ens18 | awk '{print $4}' | cut -d/ -f1)
@@ -101,7 +120,8 @@ ${F1}       Processes ${F2}= ${F3}$PROCCOUNT of $PROCMAX MAX${F1}"
 ## Storage Informations
 function show_storage_info () {
 
-    if [ "$STORAGE_INFO" = "1" ]; then
+    if [[ "$STORAGE_INFO" = "Y" || "$STORAGE_INFO" = "y" ]]; then
+
         STORAGE=$(df -hT |sort -r -k 6 -i |sed -e 's/^File.*$/\x1b[0;37m&\x1b[1;32m/' |sed -e 's/^Datei.*$/\x1b[0;37m&\x1b[1;32m/' |egrep -v docker )
 
 ## display storage information
@@ -115,7 +135,8 @@ ${F3}${STORAGE}${F1}"
 ## User Informations
 function show_user_info () {
 
-    if [ "$USER_INFO" = "1" ]; then
+    if [[ "$USER_INFO" = "Y" || "$USER_INFO" = "y" ]]; then
+
         WHOIAM=$(whoami)
         GROUPZ=$(groups)
         ID=$(id)
@@ -123,8 +144,8 @@ function show_user_info () {
         LOGGEDIN=$(echo $(who |awk {'print $1" " $5'} |awk -F '[()]' '{ print $1 $2 '}  |uniq -c |awk {'print "(" $1 ") "$2" " $3","'} ) |sed 's/,$//' |sed '1,$s/\([^,]*,[^,]*,[^,]*,\)/\1\n\\033[1;32m\t          /g')
         SYSTEMUSERCOUNT=$(cat /etc/passwd |egrep '\:x\:10[0-9][0-9]' |grep '\:\/bin\/bash' |wc -l)
         SYSTEMUSER=$(cat /etc/passwd |egrep '\:x\:10[0-9][0-9]' |egrep '\:\/bin\/bash|\:\/bin/sh' |awk '{if ($0) print}' |awk -F ':' {'print $1'} |awk -vq=" " 'BEGIN{printf""}{printf(NR>1?",":"")q$0q}END{print""}' |cut -c2- |sed 's/ ,/,/g' |sed '1,$s/\([^,]*,[^,]*,[^,]*,[^,]*,[^,]*,\)/\1\n\\033[1;32m\t          /g')
-        SUPERUSERCOUNT=$(cat /root/.ssh/authorized_keys |egrep '^ssh-' |wc -l)
-        SUPERUSER=$(cat /root/.ssh/authorized_keys |egrep '^ssh-' |awk '{print $NF}' |awk -vq=" " 'BEGIN{printf""}{printf(NR>1?",":"")q$0q}END{print""}' |cut -c2- |sed 's/ ,/,/g' |sed '1,$s/\([^,]*,[^,]*,[^,]*,\)/\1\n\\033[1;32m\t          /g' )
+        SUPERUSERCOUNT=$(cat $HOME/.ssh/authorized_keys |egrep '^ssh-' |wc -l)
+        SUPERUSER=$(cat $HOME/.ssh/authorized_keys |egrep '^ssh-' |awk '{print $NF}' |awk -vq=" " 'BEGIN{printf""}{printf(NR>1?",":"")q$0q}END{print""}' |cut -c2- |sed 's/ ,/,/g' |sed '1,$s/\([^,]*,[^,]*,[^,]*,\)/\1\n\\033[1;32m\t          /g' )
         KEYUSERCOUNT=$(for i in $(cat /etc/passwd |egrep '\:x\:10[0-9][0-9]' |awk -F ':' {'print $6'}) ; do cat $i/.ssh/authorized_keys  2> /dev/null |grep ^ssh- |awk '{print substr($0, index($0,$3)) }'; done |wc -l)
         KEYUSER=$(for i in $(cat /etc/passwd |egrep '\:x\:10[0-9][0-9]' |awk -F ':' {'print $6'}) ; do cat $i/.ssh/authorized_keys  2> /dev/null |grep ^ssh- |awk '{print substr($0, index($0,$3)) }'; done |awk -vq=" " 'BEGIN {printf ""}{printf(NR>1?",":"")q$0q}END{print""}' |cut -c2- |sed 's/ , /, /g' |sed '1,$s/\([^,]*,[^,]*,[^,]*,\)/\1\n\\033[1;32m\t          /g'  )
 
@@ -144,22 +165,14 @@ ${F1}      SshKeyUsers ${F2}= ${F3}[$KEYUSERCOUNT] $KEYUSER${F1}"
 ## Environment Informations
 function show_environment_info () {
 
-    if [ "$ENVIRONMENT_INFO" = "1" ]; then
-        if [ ! -f $ENVFILE ]; then
-            createenv ;
-        fi
-        source $ENVFILE
-        if [ -z "${SYSFUNCTION}" ] || [ -z "${SYSENV}" ] || [ -z "${SYSSLA}" ] || [ -z "${SYSNOTE}" ]; then
-            rm $ENVFILE
-            createenv ;	
-        fi
+    if [[ "$ENVIRONMENT_INFO" = "Y" || "$ENVIRONMENT_INFO" = "y" ]]; then
 
 ## display environment information
 echo -e "
 ${F2}============[ ${F1}Environment Data${F2} ]===============================================
 ${F1}        Function ${F2}= ${F3}$SYSFUNCTION
 ${F1}     Environment ${F2}= ${F3}$SYSENV
-${F1}   Service Level ${F2}= ${F3}$SYSSLA${F1}"
+${F1}   Service Level ${F2}= ${F3}$SYSSLA${F1}
 ${F1}            NOTE ${F2}= ${F3}$SYSNOTE${F1}"
 
     fi
@@ -184,7 +197,6 @@ clear
     show_storage_info
     show_user_info
     show_end
-
 }
 
 #### Main Part
